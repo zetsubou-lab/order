4c9a6119f7b724c1b24ffd34e4bddacc8ec714ca
zetsubou-lab <52990563+zetsubou-lab@users.noreply.github.com>
2021-01-22 10:57:20 +0800

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
  
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
-export PROJ_PATH=/usr/java/tomcat/apache-tomcat-9.0.41/webapps/jenkins/workspace
+export PROJ_PATH=/usr/java/tomcat/apache-tomcat-9.0.41/webapps/jenkins/workspace/DeployOrder
  
 # 输入你的环境上tomcat的全路径
 export TOMCAT_APP_PATH=/usr/java/tomcat/apache-tomcat-9.0.41
